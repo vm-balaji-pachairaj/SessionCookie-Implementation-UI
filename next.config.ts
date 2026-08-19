7f6ddd7fb87999078c9c226d0eed38b97fa14e47
Vignesh Sivaraj <Vignesh.Sivaraj@valuemomentum.com>
2026-08-19 22:55:06 +0530

    Add API proxy rewrites to next.config.ts for backend integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      // {
+      //   source: "/api/refresh",
+      //   destination: "http://localhost:5000/refresh",
+      // },
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/:path*", // Proxy to backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
